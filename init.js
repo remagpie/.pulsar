260e3138e1db762048593f5e255d86f9279b2b01
Joonmo Yang <dev@remagpie.com>
2023-12-28 04:31:04 +0900

    Add basic settings

--- init.js
+++ init.js
@@ -1,17 +1,27 @@
-// Your init script
-//
-// Pulsar will evaluate this file each time a new window is opened. It is run
-// after packages are loaded/activated and after the previous editor state
-// has been restored.
-//
-// An example hack to log to the console when each text editor is saved.
-//
-// atom.workspace.observeTextEditors(editor =>
-//   editor.onDidSave(() =>
-//     console.log(`Saved! ${editor.getPath()}`)
-//   )
-// );
-//
-// See the Pulsar Launch manual for more information on this file and how to
-// customize it.
-// https://pulsar-edit.dev/docs/launch-manual/sections/core-hacking/#the-init-file
+"use strict";
+
+// Disable some builtin packages
+atom.packages.disablePackage("about");
+atom.packages.disablePackage("archive-view");
+atom.packages.disablePackage("autocomplete-atom-api");
+atom.packages.disablePackage("autoflow");
+atom.packages.disablePackage("autosave");
+atom.packages.disablePackage("background-tips");
+atom.packages.disablePackage("bookmarks");
+atom.packages.disablePackage("github");
+atom.packages.disablePackage("go-to-line");
+atom.packages.disablePackage("link");
+atom.packages.disablePackage("metrics");
+atom.packages.disablePackage("open-on-github");
+atom.packages.disablePackage("spell-check");
+atom.packages.disablePackage("update-package-dependencies");
+atom.packages.disablePackage("welcome");
+
+atom.config.set("core.autoHideMenuBar", true);
+atom.config.set("core.automaticallyUpdate", false);
+atom.config.set("core.closeEmptyWindows", false);
+atom.config.set("editor.invisibles.eol", " ");
+atom.config.set("editor.scrollPastEnd", true);
+atom.config.set("editor.showIndentGuide", true);
+atom.config.set("editor.showInvisibles", true);
+atom.config.set("editor.tabLength", 4);
---
+++ src/ppm.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const { BufferedProcess } = require("atom");
+const fs = require("fs").promises;
+const path = require("path");
+
+function isObject(val) {
+	return (typeof val === "object") && val !== null;
+}
+
+// Execute ppm command. Turns off the color by default.
+async function ppm(...args) {
+	return await new Promise((resolve, reject) => {
+		let output = "";
+		let error = "";
+		new BufferedProcess({
+			command: atom.packages.getApmPath(),
+			args: ["--no-color", ...args],
+			stdout: (o) => { output += o },
+			stderr: (e) => { error += e },
+			exit: (code) => {
+				if (code === 0) {
+					resolve(output);
+				} else {
+					reject(error);
+				}
+			},
+		});
+	});
+}
+
+const installing = new Map();
+async function install(name, parents = []) {
+	// Just enable the package if it's already installed
+	if (atom.packages.getAvailablePackageNames().includes(name)) {
+		atom.packages.enablePackage(name);
+		return;
+	}
+
+	// Return immediately if it's already installing
+	if (installing.has(name)) {
+		return;
+	}
+
+	installing.set(name, ppm("install", name, "--color"));
+	const notification = atom.notifications.addInfo(`Installing "${name}"`, {
+		detail: `Required by ${parents.join(" > ")}`,
+		dismissable: true,
+	});
+	try {
+		await installing.get(name);
+	} catch (e) {
+		atom.notifications.addError(`Failed to install "${name}"`, {
+			detail: e,
+		});
+		throw e;
+	} finally {
+		notification.dismiss();
+		installing.delete(name);
+	}
+
+	// Find the package path and load package.json
+	let metadata;
+	for (const dir of atom.packages.getPackageDirPaths()) {
+		const packages = await fs.readdir(dir);
+		if (packages.includes(name)) {
+			metadata = require(path.join(dir, name, "package.json"));
+			break;
+		}
+	}
+
+	// Refresh theme list if the package is theme package
+	if (Object.keys(metadata).includes("theme")) {
+		console.log("activating");
+		await atom.themes.activateThemes();
+	}
+
+	// Install the dependencies
+	if (Object.keys(metadata).includes("package-deps")) {
+		metadata["package-deps"].forEach((p) => install(p, [...parents, name]));
+	}
+}
+
+function configure(key, scope, config) {
+	if (isObject(config)) {
+		for (const [k, v] of Object.entries(config)) {
+			configure(key + "." + k, scope, v);
+		}
+	}
+	else {
+		atom.config.set(key, config, {
+			scopeSelector: scope !== "*" ? scope : undefined,
+		});
+	}
+}
+
+async function activate(name, config) {
+	await install(name);
+	configure(name, "*", config);
+}
+
+exports.activate = activate;
+exports.configure = configure;
+exports.install = install;
